8dfc3eac9c70bb385572c504453ae81c58e352e0
Paul Potsides <paul@zero-1.co>
2020-04-03 22:06:01 +0200

    Level 2 more interesting.

--- src/levels/2.tsx
+++ src/levels/2.tsx
@@ -8,13 +8,68 @@ export default {
     },
     {
       to: "3",
-      x: 400,
+      x: 1450,
       y: 0
     }
   ],
-  enemies: [],
-  guns: [],
+  enemies: [
+    {
+      hp: 4,
+      maxX: 600,
+      minX: 500,
+      speed: 2,
+      x: 560,
+      y: 35
+    },
+    {
+      hp: 4,
+      maxX: 750,
+      minX: 650,
+      speed: 2,
+      x: 700,
+      y: 0
+    },
+    {
+      hp: 4,
+      maxX: 1300,
+      minX: 1000,
+      speed: 4,
+      x: 1250,
+      y: 0
+    }
+  ],
+  guns: [
+    {
+      x: 50,
+      y: 0
+    }
+  ],
   name: "2",
-  platforms: [],
+  platforms: [
+    {
+      height: 20,
+      width: 120,
+      x: 500,
+      y: 15
+    },
+    {
+      height: 20,
+      width: 90,
+      x: 250,
+      y: 40
+    },
+    {
+      height: 20,
+      width: 90,
+      x: 360,
+      y: 80
+    },
+    {
+      height: 20,
+      width: 50,
+      x: 800,
+      y: 20
+    }
+  ],
   width: 1500
 };
